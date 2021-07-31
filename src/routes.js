d74d950fe2b4aa471c424bf53555802f26cf4ad7
Alvan <achonwaalvan@gmail.com>
2021-07-31 16:48:55 +0100

    update route file with comments

--- src/routes.js
+++ src/routes.js
@@ -66,94 +66,90 @@ const Routes = () => {
 
             {/* Student Routes */}
             <Route path="/student/signup" exact component={StudentSignup} />
-            <Route path="/student/login" component={StudentLogin} />
-            <RoleRoute path="/student/dashboard" component={StudentDashboard} />
+            <Route path="/student/login" component={StudentLogin} /> {/* Student login */}
+            <RoleRoute path="/student/dashboard" component={StudentDashboard} />  {/* Student's dashboard with their chapters */}
             <RoleRoute
               path="/student/profile/avatar"
               component={AvatarUpload}
-            />
+            />  {/* Student profile upload */}
             <RoleRoute
               path="/student/project/setup"
               component={ProjectDetails}
-            />
+            />  {/* Student project setup */}
             <RoleRoute
               path="/student/project/upload"
               component={ProjectUpload}
-            />
+            />  {/* Student's project PDF upload */}
             <RoleRoute
               path="/student/project/chapters/:chapter_id"
               component={ProjectDetails}
-            />
-            <RoleRoute
-              path="/student/project/pairing/:supervisor_id"
-              component={SupervisorPairing}
-            />
+            />  {/* Student's chapter view with all pages */}
             <RoleRoute
               path="/student/project/status"
               component={ProjectStatus}
-            />
+            />  {/* Student's project status. Whether the project has been accepted or not */}
             <RoleRoute
               path="/student/chapters/:chapter_id"
               component={ProjectChapter}
-            />
+            />  {/* Student's single chapter view */}
 
             {/* Supervisor Routes */}
-            <Route path="/supervisor/login" exact component={SupervisorLogin} />
+            <Route path="/supervisor/login" exact component={SupervisorLogin} />  {/* Supervisor's login */}
             <RoleRoute
               path="/supervisor/password-update"
               component={PasswordUpdate}
-            />
+            />  {/* Supervisor's password update view */}
             <RoleRoute
               path="/supervisor/avatar-upload"
               component={AvatarUpload}
-            />
+            />  {/* Supervisor's avatar upload */}
             <RoleRoute
               path="/supervisor/dashboard"
               component={SupervisorDashboard}
-            />
+            />  {/* Supervisor's dashboard with students assigned to him */}
             <RoleRoute
               path="/supervisor/recent-proposals"
               exact
               component={RecentProposals}
-            />
+            />  {/* Supervisor's view of all recent proposals */}
             <RoleRoute
               path="/supervisor/recent-proposals/:proposal_id"
               exact
               component={RecentProposal}
-            />
+            />  {/* Supervisor's view of a single student's proposals */}
             <RoleRoute
               path="/supervisor/students/:student_id"
               exact
               component={StudentProject}
-            />
+            />  {/* Supervisor viewing a student's chapters */}
             <RoleRoute
               path="/supervisor/students/:student_id/chapters/:chapter_id"
               exact
               component={StudentProjectChapter}
-            />
+            />  {/* Supervisor view of a student's chapter */}
 
             {/* Coordinator Routes */}
-            <Route path="/coordinator/signup" component={CoordinatorSignup} />
-            <Route path="/coordinator/login" component={CoordinatorLogin} />
+            <Route path="/coordinator/signup" component={CoordinatorSignup} />  {/* Coordinator Signup */}
+            <Route path="/coordinator/login" component={CoordinatorLogin} />  {/* Coordinator login */}
             <RoleRoute
               path="/coordinator/project-categories"
               component={ProjectCategories}
               exact
-            />
+            />  {/* Viewing available project categories */}
             <RoleRoute
               path="/coordinator/project-categories/add"
               component={CreateProjectCategory}
               exact
-            />
+            />  {/* Creating a new project category */}
             <RoleRoute
               exact
               path="/coordinator/dashboard"
               component={CoordinatorDashboard}
-            />
+            />  {/* Coordinator's Dashboard view */}
             <RoleRoute
               path="/coordinator/supervisors/:supervisor_id/students"
               component={SupervisorStudents}
-            />
+            />  {/* Coordinator Viewing Supervisor Students  */}
 
             <Route path="*" component={NotFound} />
           </Switch>
